fff6c1398d551e64901ddec7364e487e93f9f166
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-03-22 13:49:09 +0100

    Jeu du morpion (react) : Jouer tour à tour

--- morpion-react/src/index.js
+++ morpion-react/src/index.js
@@ -2,17 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick({ value: "X" })}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => this.props.onClick({ value: "X" })}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -20,13 +28,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -39,7 +51,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Prochain joueur: X";
+    const status = "Prochain joueur : " + (this.state.xIsNext ? "X" : "O");
 
     return (
       <div>
